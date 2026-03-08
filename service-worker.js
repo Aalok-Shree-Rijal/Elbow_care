6ada837b61c87e8348eacf5f0aab3beee5d1f4d7
Aalok-Shree-Rijal <holyshot195@gmail.com>
2026-03-08 18:21:28 +0545

    debugging_relative_file_path

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 const CACHE_NAME = 'elbowcare-v2';
 const urlsToCache = [
-  '/Elbow_care/',
-  '/Elbow_care/index.html',
-  '/Elbow_care/app.js',
-  '/Elbow_care/style.css',
-  '/Elbow_care/manifest.json',
-  '/Elbow_care/icon-192.png',
-  '/Elbow_care/icon-512.png',
-  '/Elbow_care/exercise1.mp4',
-  '/Elbow_care/exercise2.mp4',
-  '/Elbow_care/exercise3.mp4',
-  '/Elbow_care/exercise4.mp4',
-  '/Elbow_care/buzzer.mp3',
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './exercise1.mp4',
+  './exercise2.mp4',
+  './exercise3.mp4',
+  './exercise4.mp4',
+  './buzzer.mp3',
 ];
 
 self.addEventListener('install', event => {
