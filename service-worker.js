8b5661d22f6133afa27f7183f240daf2b62312c5
Aalok-Shree-Rijal <holyshot195@gmail.com>
2026-03-08 17:07:38 +0545

    video_playback_error_offline

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,15 @@ const CACHE_NAME = 'elbowcare-v1';
 const urlsToCache = [
   '/Elbow_care/',
   '/Elbow_care/index.html',
-  // add your CSS/JS files here too
+  '/Elbow_care/app.js',
+  '/Elbow_care/style.css',
+  '/Elbow_care/manifest.json',
+  '/Elbow_care/icon-192.png',
+  '/Elbow_care/icon-512.png',
+  '/Elbow_care/exercise1.mp4',
+  '/Elbow_care/exercise2.mp4',
+  '/Elbow_care/exercise3.mp4',
+  '/Elbow_care/exercise4.mp4',
 ];
 
 self.addEventListener('install', event => {
